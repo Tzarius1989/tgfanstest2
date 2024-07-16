0008417906c5cfdf7ea1faad8671f42aca8b7220
Tzarius1989 <73390651+Tzarius1989@users.noreply.github.com>
2024-07-16 19:14:51 +0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,9 @@
+import { fileURLToPath } from 'url';
+import { dirname, resolve } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -8,11 +14,10 @@ const nextConfig = {
   webpack: (config) => {
     config.resolve.alias = {
       ...config.resolve.alias,
-      '@': path.resolve(__dirname),
+      '@': resolve(__dirname),
     };
     return config;
   },
 };
 
-import path from 'path';
 export default nextConfig;
