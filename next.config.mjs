89258afbfd5e68b7482a2c3fc8fc3340835c9a74
Tzarius1989 <73390651+Tzarius1989@users.noreply.github.com>
2024-07-16 19:34:55 +0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -6,7 +6,11 @@ const __dirname = dirname(__filename);
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // ... другие настройки ...
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: ['via.placeholder.com'],
+  },
   webpack: (config) => {
     config.resolve.alias = {
       ...config.resolve.alias,
